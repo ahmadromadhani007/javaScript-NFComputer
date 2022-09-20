d35c2992379afb657ffd7b3db4d54bbc2f9a39b9
ahmadromadhani007 <dan10kiy@gmail.com>
2022-09-20 08:34:46 +0700

    updateGes

--- js/calc.js
+++ js/calc.js
@@ -1,10 +1,11 @@
 function hitung(opr){
-    //tangkap id form
+    //tangkap request element2 form
     let frm = document.getElementById('calcForm');
     let a1 = parseFloat(frm.angka1.value);
     let a2 = parseFloat(frm.angka2.value);
     if (isNaN(a1) || isNaN(a2)) return alert('Masukkan angkanya !');
 
+    //struktur kendali untuk mengoperasikan tombol2
     switch (opr) {
         case '+': total = a1 + a2; break;
         case '-': total = a1 - a2; break;
@@ -15,5 +16,6 @@ function hitung(opr){
         
     }
 
+    //menepatkan hasil operasi aritmatika pada element form hasil
     frm.hasil.value = total; //penempatan hasil
 }
